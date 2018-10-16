529698f2ec5168d96e4e8e47fa29904cc93b10ac
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-10-16 23:50:27 +0300

    master&follower infinite

--- Follower_run.java
+++ Follower_run.java
@@ -14,6 +14,7 @@ public class Follower_run {
         // create a follower instance
 
         follower = new Follower(SERVER_ADDRESS, SERVER_PORT);
+
         follower.initialize_connection();
 
         if (follower.socket == null) {
@@ -26,13 +27,63 @@ public class Follower_run {
 
             // update with master
 
+            new Thread(new Master_Connecter(follower, 10)).start();
+
+            // follower.terminate_connection();
+
+        }
+
+    }
+
+
+}
+
+
+
+
+class Master_Connecter implements Runnable{
+
+    Follower follower;
+    int timeout_ms;
+
+    public Master_Connecter(Follower follower, int timeout) {
+        this.follower = follower;
+        this.timeout_ms = timeout * 1_000;
+    }
+
+    @Override
+    public void run() {
+
+
+        while (true) {
+
+
             if (Resources.is_changed()) {
 
                 // send files only if changes are present
 
                 System.out.println("Follower : changes are present.");
 
-                send();
+                do {
+
+                    System.out.println("Follower : is sending..");
+
+                    //client sends ClientSend command, and the details of transaction
+
+                    String command = "ClientSend";
+                    follower.send_command(command);
+                    follower.send_command(Resources.get_changes_names());
+                    follower.send_command(Resources.get_changes_sizes());
+                    follower.send_command(Resources.get_checksums());
+
+                    //send changes until success
+
+                    Resources.send_files(follower.socket, Resources.get_changes_files());
+                    System.out.println("Follower send files.");
+
+                } while (!follower.get_response().equals("MasterReceived"));
+
+                System.out.flush();
 
             } else {
 
@@ -42,7 +93,29 @@ public class Follower_run {
 
             // receive data for changes in master
 
-            receive();
+            boolean success;
+
+            do {
+
+                System.out.println("Follower : is receiving..");
+
+                //client sends ClientReceive command, and gets the details of transaction
+
+                String command = "ClientReceive";
+                follower.send_command(command);
+                String filesToReceive = follower.get_response();
+                String size_filesToReceive = follower.get_response();
+                String fileChecksums = follower.get_response();
+
+                //ask for changes until success
+
+                success = Resources.receive_files(
+                        follower.socket,
+                        filesToReceive,
+                        size_filesToReceive,
+                        fileChecksums);
+
+            } while (!success);
 
             // end connection
 
@@ -53,70 +126,19 @@ public class Follower_run {
                 Resources.create_metafile();
 
             } catch (IOException e) {
-                System.out.println("Follower : could not create metafile.");
+                System.out.println("Follower : could not create metafile");
             }
 
             follower.send_command("ClientDone");
 
-            follower.terminate_connection();
+            try {
+                Thread.sleep(timeout_ms);
+            } catch (InterruptedException e) {
+                System.out.println("Master_Connector thread interrupted");
+            }
 
         }
 
     }
-
-
-    public static void send() {
-
-        do {
-
-            System.out.println("Follower : is sending..");
-
-            //client sends ClientSend command, and the details of transaction
-
-            String command = "ClientSend";
-            follower.send_command(command);
-            follower.send_command(Resources.get_changes_names());
-            follower.send_command(Resources.get_changes_sizes());
-            follower.send_command(Resources.get_checksums());
-
-            //send changes until success
-
-            Resources.send_files(follower.socket, Resources.get_changes_files());
-            System.out.println("Follower send files.");
-
-        } while (!follower.get_response().equals("MasterReceived"));
-
-        System.out.flush();
-    }
-
-
-    public static void receive() {
-
-        boolean success;
-
-        do {
-
-            System.out.println("Follower : is receiving..");
-
-            //client sends ClientReceive command, and gets the details of transaction
-
-            String command = "ClientReceive";
-            follower.send_command(command);
-            String filesToReceive = follower.get_response();
-            String size_filesToReceive = follower.get_response();
-            String fileChecksums = follower.get_response();
-
-            //ask for changes until success
-
-            success = Resources.receive_files(
-                        follower.socket,
-                        filesToReceive,
-                        size_filesToReceive,
-                        fileChecksums);
-
-        } while (!success);
-
-    }
-
 
 }
